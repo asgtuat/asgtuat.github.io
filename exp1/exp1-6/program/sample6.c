5faf6bb9d2c0561f8c64adc9013f470ab55afe62
Hiroshi Yamada <boro2@hiroshi-yamada-no-powerbook5.local>
2017-11-07 09:22:17 +0900

    nits

--- exp1/exp1-6/program/sample6.c
+++ exp1/exp1-6/program/sample6.c
@@ -5,13 +5,13 @@ int main()
 	int a[10], b[10], i;
 	
 	for (i = 0; i < 10; i++)
-		b[i] = 0;
+		a[i] = 0;
 
 	for (i = 0; i < 20; i++)
-		a[i] = 1;
+		b[i] = 1;
 
 	for (i = 0; i < 10; i++)
-		printf("%d\n", b[i]);
+		printf("%d\n", a[i]);
 	
 	return 0;
 }
