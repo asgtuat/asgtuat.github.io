466a19cfe8ef86f10580a0be61328b2fdf2c1e18
Hiroshi Yamada <hiroshiy@cc.tuat.ac.jp>
2024-11-12 13:39:49 +0900

    nits

--- exp1/exp1-asm/program/assignment1.c
+++ exp1/exp1-asm/program/assignment1.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
-static int count;
+
 int fibonacci(int a)
 {
 	int ret;
-	count++;
 	if (a == 0)      ret = 0;
 	else if (a == 1) ret = 1;
 	else             ret = fibonacci(a - 2) + fibonacci(a - 1);
@@ -17,6 +16,6 @@ int main()
 
 	a = fibonacci(4);
 	printf("%d\n", a);
-	printf("%d\n", count);
+
 	return 0;
 }
